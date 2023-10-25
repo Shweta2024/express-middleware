02a6836ae84f629ea77cbb01614906c9d546379e
Shweta Bhagat <bhagatshweta0216@gmail.com>
2023-10-25 10:54:07 +0530

    add routes

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 5000
 
+const homeRoute = require('./routes/home')
+const usersRoute = require('./routes/users')
+
+app.use(homeRoute)
+app.use(usersRoute)
+
 app.listen(PORT, (req, res) => {
     console.log(`server started at port: ${PORT}`)
 })
